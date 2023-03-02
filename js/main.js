170498fc5a471aade55481c1b37fc85b90edde0d
Konstantin Grinev <konstantingrinev7@gmail.com>
2023-03-02 11:50:29 +0300

    index.html, main.js, navbar.scss: -redo navbar functionality

--- js/main.js
+++ js/main.js
@@ -2,17 +2,10 @@
 
 // NAVBAR TOGGLER
 const navbarBtn = document.querySelector(".navbar__btn");
-const burgerIcon = document.querySelector(".navbar__btn--burger");
-const closeIcon = document.querySelector(".navbar__btn--close");
-const navColumn = document.querySelector(".navbar__nav--column");
-const navRow = document.querySelector(".navbar__nav--row");
-const docWidth = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
 
 function toggleMenu() {
-  navColumn.classList.toggle("d-none");
-  burgerIcon.classList.toggle("d-none");
-  closeIcon.classList.toggle("d-none");
-  console.log(navbarBtn.children);
+  navbar.classList.toggle("navbar__nav-display");
 }
 
 navbarBtn.addEventListener("click", toggleMenu);
